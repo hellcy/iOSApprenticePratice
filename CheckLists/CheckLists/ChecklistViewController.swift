94cc0142d377ee0d9111b5fccc31705bb363f65f
Yuan Cheng <chengyuan82281681@hotmail.com>
2019-07-02 17:08:16 +1000

    update

--- CheckLists/CheckLists/ChecklistViewController.swift
+++ CheckLists/CheckLists/ChecklistViewController.swift
@@ -55,36 +55,18 @@ class ChecklistViewController: UITableViewController {
     override func tableView(_ tableView: UITableView,           // parameter 1: underscore is used when we don't want a external name, external name is the name we used to call the method, we often set first parameter name to _ when the methods are come from frameworks
                             numberOfRowsInSection section: Int) // parameter 2: numberOfRowsInSection is the external name, section is the local parameter name, we use this name in the method
                             -> Int {                            // return value
-        return 5
+        return items.count
     }
     
     // then the UITableView asks for the data to display on that cell, so we return a copy of the cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
         
+        let item = items[indexPath.row]
+        
         // using tags is the same as using @IBOutlet to refernce object in the storyboard
         let label = cell.viewWithTag(1000) as! UILabel
-        
-        if indexPath.row == 0
-        {
-            label.text = row0item.text
-        }
-        else if indexPath.row == 1
-        {
-            label.text = row1item.text
-        }
-        else if indexPath.row == 2
-        {
-            label.text = row2item.text
-        }
-        else if indexPath.row == 3
-        {
-            label.text = row3item.text
-        }
-        else if indexPath.row == 4
-        {
-            label.text = row4item.text
-        }
+        label.text = item.text
         configCheckmark(for: cell, at: indexPath)
         return cell
     }
@@ -95,21 +77,8 @@ class ChecklistViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let cell = tableView.cellForRow(at:  indexPath)
         {
-            if indexPath.row == 0 {
-                row0item.checked = !row0item.checked
-            }
-            else if indexPath.row == 1 {
-                row1item.checked = !row1item.checked
-            }
-            else if indexPath.row == 2 {
-                row2item.checked = !row2item.checked
-            }
-            else if indexPath.row == 3 {
-                row3item.checked = !row3item.checked
-            }
-            else if indexPath.row == 4 {
-                row4item.checked = !row4item.checked
-            }
+            let item = items[indexPath.row]
+            item.checked = !item.checked
             configCheckmark(for: cell, at: indexPath)
 
         }
@@ -120,24 +89,9 @@ class ChecklistViewController: UITableViewController {
     
     func configCheckmark(for cell: UITableViewCell, at indexPath: IndexPath)
     {
-        var isChecked = false
-        if indexPath.row == 0 {
-            isChecked = row0item.checked
-        }
-        else if indexPath.row == 1 {
-            isChecked = row1item.checked
-        }
-        else if indexPath.row == 2 {
-            isChecked = row2item.checked
-        }
-        else if indexPath.row == 3 {
-            isChecked = row3item.checked
-        }
-        else if indexPath.row == 4 {
-            isChecked = row4item.checked
-        }
+        let item = items[indexPath.row]
         
-        if isChecked {
+        if item.checked {
             cell.accessoryType = .checkmark
         } else {
             cell.accessoryType = .none
