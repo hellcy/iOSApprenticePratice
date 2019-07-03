c71afb7076567ac8d9ea3eeffd8775b316ffad9b
Yuan Cheng <yuan@unicard.com.au>
2019-07-03 20:26:29 +1000

    edit checklists

--- CheckLists/CheckLists/ListDetailViewController.swift
+++ CheckLists/CheckLists/ListDetailViewController.swift
@@ -37,4 +37,44 @@ class ListDetailViewController: UITableViewController, UITextFieldDelegate {
             doneBarButton.isEnabled = true
         }
     }
+    
+    // focus on the text field when first enter the view
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    // MARK:- TableView Delegates
+    // disable selection of table cells
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    // MARK:- Actions
+    @IBAction func cancel() {
+        delegate?.listDetailViewControllerDidCancel(self)
+    }
+    
+    // if checklistToEdit is not nil, send its value to its delegate
+    @IBAction func done() {
+        if let checklist = checklistToEdit {
+            checklist.name = textField.text!
+            delegate?.listDetailViewController(self, didFinishEditing: checklist)
+        } else {
+            let checklist = Checklist(name: textField.text!)
+            delegate?.listDetailViewController(self, didFinishAdding: checklist)
+        }
+    }
+    
+    // MARK:- UITextField Delegates
+    // check if text field is empty
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange,
+                                                  with: string)
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+    }
 }
