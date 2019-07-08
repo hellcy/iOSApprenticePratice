a1fd818c374de75fac7fe23ed0c5a304daa4dcb9
Yuan Cheng <chengyuan82281681@hotmail.com>
2019-07-08 14:22:44 +1000

    location with timer

--- MyLocations/MyLocations/CurrentLocationViewController.swift
+++ MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -19,11 +19,24 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     @IBOutlet weak var getButton: UIButton!
     
     let locationManager = CLLocationManager()
+    var location: CLLocation?
+    var updatingLocation = false // flag to check if app is now updating locations
+    var lastLocationError: Error?
+    
+    // reverse gencodeing variables
+    let geocoder = CLGeocoder()
+    var placemark: CLPlacemark?
+    var performingReverseGeocoding = false
+    var lastGeocodingError: Error?
+    
+    // track the time for updating location
+    var timer: Timer?
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateLabels()
     }
     
     // MARK: - Action methods
@@ -35,30 +48,236 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
             locationManager.requestWhenInUseAuthorization()
             return
         }
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-        locationManager.startUpdatingLocation()
+        
+        // display alert if don't have user's permission on using location services
+        if authStatus == .denied || authStatus == .restricted {
+            showLocationServicesDeniedAlert()
+            return
+        }
+        
+        if updatingLocation {
+            stopLocationManager()
+        } else {
+            location = nil
+            lastLocationError = nil // since we may restart getting locations, so clear out the previous error
+            startLocationManager()
+        }
+        updateLabels()
     }
     
     // MARK: - CLLocationManagerDelegate
     // when app wants to get location but doesn't have access to it(user denied)
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("didFailWithError \(error)")
+        
+        if (error as NSError).code ==
+            CLError.locationUnknown.rawValue {
+            return
+        }
+        // if error is some serious error, stop updating locations.
+        lastLocationError = error
+        stopLocationManager()
+        updateLabels()
     }
     
-    // when app wants to update current location
+    // when app got the current location
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let newLocation = locations.last!
         print("didUpdateLocations \(newLocation)")
+        // 1. because getting locations consume battery power a lot, so we don't want to call this service too often, if last location is got less than 5 seconds ago, we simply end the method by return nothing.
+        if newLocation.timestamp.timeIntervalSinceNow < -5 {
+            return
+        }
+        // 2. if the location results accuracy less than 0, it doesn't make any sense, so we ignore these.
+        if newLocation.horizontalAccuracy < 0 {
+            return
+        }
+        
+        var distance = CLLocationDistance(Double.greatestFiniteMagnitude)
+        if let location = location {
+            distance = newLocation.distance(from: location)
+        }
+        // 3. if this is the first time we get location, location variable might be nil at first. Or if the newLocation accuracy is better than the current accuracy, we replace the new location results with the current location results.
+        if location == nil || location!.horizontalAccuracy >
+            newLocation.horizontalAccuracy {
+            // 4. update location results and clear out previous error bacause this time we successfully got location results
+            lastLocationError = nil
+            location = newLocation
+            // 5. if the new location results accuracy is better than the desired accuracy, than we finish updating the locations
+            if newLocation.horizontalAccuracy <=
+                locationManager.desiredAccuracy {
+                print("*** We're done!")
+                stopLocationManager()
+                
+                if distance > 0 {
+                    performingReverseGeocoding = false
+                }
+            }
+            
+            if !performingReverseGeocoding {
+                print("*** Going to geocode \(distance)")
+                performingReverseGeocoding = true
+                // Closure: compeltionHandler is where you call the location services and got values in placeamrks and error. It is like a callback method in other languages, where user can also do other things with the app while it is getting location results. The difference between the delegate methods and closure is that you can put all reletive variables in the same place, whereas delegate has to be in separate methods.
+                geocoder.reverseGeocodeLocation(newLocation, completionHandler: {
+                    placemarks, error in
+                    self.lastGeocodingError = error
+                    if error == nil, let p = placemarks, !p.isEmpty {
+                        self.placemark = p.last!
+                    } else {
+                        self.placemark = nil
+                    }
+                    self.performingReverseGeocoding = false
+                    self.updateLabels()
+                })
+            }
+            
+        }
+        // if the distance is not far from the last location and it has been more than 10 seconds since you have received last reading, than force stop because it is unlikely that you will receive any better results
+        else if distance < 1 {
+            let timeInterval = newLocation.timestamp.timeIntervalSince(location!.timestamp)
+            if timeInterval > 10 {
+                print("*** Force done!")
+                stopLocationManager()
+                updateLabels()
+            }
+        }
+        else {
+            print("\(distance)")
+        }
     }
     
+    // MARK: - Custom methods
+    // display an alert to user, when they tapped on getButton but the app doesn't have permission to use the location services
     func showLocationServicesDeniedAlert(){
         let alert = UIAlertController(
             title: "Location Services Disabled",
             message: "Please enable location services for this app in Settings.",
-            perferredStyle: .alert
+            preferredStyle: .alert
         )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default,
+                                     handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
+    
+    func updateLabels() {
+        if let location = location {
+            latitudeLabel.text = String(format: "%.8f",
+                                        location.coordinate.latitude)
+            longitudeLabel.text = String(format: "%.8f",
+                                         location.coordinate.longitude)
+            tagButton.isHidden = false
+            messageLabel.text = ""
+            
+            // display address to the label
+            if let placemark = placemark {
+                addressLabel.text = string(from: placemark)
+            } else if performingReverseGeocoding {
+                addressLabel.text = "Searching for Address..."
+            } else if lastGeocodingError != nil {
+                addressLabel.text = "Error Finding Address"
+            } else {
+                addressLabel.text = "No Address Found"
+            }
+        } else {
+            latitudeLabel.text = ""
+            longitudeLabel.text = ""
+            addressLabel.text = ""
+            tagButton.isHidden = true
+            let statusMessage: String
+            if let error = lastLocationError as NSError? {
+                if error.domain == kCLErrorDomain &&
+                    error.code == CLError.denied.rawValue {
+                    statusMessage = "Location Services Disabled" // check if user has denied app from getting locations
+                } else {
+                    statusMessage = "Error Getting Location"
+                }
+            } else if !CLLocationManager.locationServicesEnabled() {
+                statusMessage = "Location Services Disabled" // check if device has disabled location services for all apps
+            } else if updatingLocation {
+                statusMessage = "Searching..."
+            } else {
+                statusMessage = "Tap 'Get My Location' to Start"
+            }
+            messageLabel.text = statusMessage
+        }
+         configureGetButton()
+    }
+    
+    // stop updating locations, also change the updatingLocation flag to false
+    func stopLocationManager() {
+        if updatingLocation {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            updatingLocation = false
+            
+            // stop the timer when user tapped 'stop' button or desired location found before time is up
+            if let timer = timer {
+                timer.invalidate()
+            }
+        }
+    }
+    
+    // start updating locations.
+    func startLocationManager() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy =
+            kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            updatingLocation = true
+            
+            timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(didTimeOut), userInfo: nil, repeats: false)
+        }
+    }
+    
+    // config the text of getButton, if we are updating locations, change text to 'Stop', or change text to 'Get my location'
+    func configureGetButton() {
+        if updatingLocation {
+            getButton.setTitle("Stop", for: .normal)
+        } else {
+            getButton.setTitle("Get My Location", for: .normal)
+        }
+    }
+    
+    // convert CLPlacemark to string
+    func string(from placemark: CLPlacemark) -> String {
+        // 1. empty variable string for address
+        var line1 = ""
+        // 2. subThoroughfare is just a fancy name for street number
+        if let s = placemark.subThoroughfare {
+            line1 += s + " "
+        }
+        // 3. thoroughfare is the street number
+        if let s = placemark.thoroughfare {
+            line1 += s
+        }
+        // 4. locality = city, administrativeArea = state, postalCode = postal Code
+        var line2 = ""
+        if let s = placemark.locality {
+            line2 += s + " "
+        }
+        if let s = placemark.administrativeArea {
+            line2 += s + " "
+        }
+        if let s = placemark.postalCode {
+            line2 += s
+        }
+        
+        // 5. Combine two string together
+        return line1 + "\n" + line2
+    }
+    
+    // this method has @objc keyword before its name because we declared it as being accessiable from Objective-C
+    @objc func didTimeOut(){
+        print("*** Time out")
+        stopLocationManager()
+        updateLabels()
 
+        if location == nil {
+            lastLocationError = NSError(domain: "MyLocationErrorDomain", code: 1, userInfo: nil)
+        }
+    }
 }
 
