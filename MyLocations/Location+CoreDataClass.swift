1be3ce9d91aee5eb36aab3f510fe34c9c43d0795
Yuan Cheng <chengyuan82281681@hotmail.com>
2019-07-17 17:08:31 +1000

    locations tab updates with sections

--- MyLocations/Location+CoreDataClass.swift
+++ MyLocations/Location+CoreDataClass.swift
@@ -9,8 +9,24 @@
 
 import Foundation
 import CoreData
+import MapKit
 
 @objc(Location)
-public class Location: NSManagedObject {
-
+public class Location: NSManagedObject, MKAnnotation {
+    // to make Location conforms to the MKAnnotation protocol, we need 3 properties: coordicate, title and  subtitle. These variables are all associated with a block of code, they are read-only computed properties, they don't actually store a value in a memory location until you access to them, then they perform their code blocks. They are read-only because they only return a value, you can't assign new values to them.
+    public var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2DMake(latitude, longitude)
+    }
+    
+    public var title: String? {
+        if locationDescription.isEmpty {
+            return "(No Description)"
+        } else {
+            return locationDescription
+        }
+    }
+    
+    public var subtitle: String? {
+        return category
+    }
 }
---
+++ MyLocations/MyLocations/MapViewController.swift
@@ -0,0 +1,147 @@
+//
+//  MapViewController.swift
+//  MyLocations
+//
+//  Created by yuan cheng on 17/7/19.
+//  Copyright © 2019 Yuan Cheng. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreData
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var managedObjectContext: NSManagedObjectContext! {
+        didSet {
+            NotificationCenter.default.addObserver(forName: Notification.Name.NSManagedObjectContextObjectsDidChange, object: managedObjectContext, queue: OperationQueue.main) {
+                notification in
+                if self.isViewLoaded {
+                    self.updateLocations()
+                }
+            }
+        }
+    }
+    
+    var locations = [Location]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateLocations()
+        if !locations.isEmpty {
+            showLocations()
+        }
+    }
+    
+    // MARK: - Actions
+    @IBAction func showUser() {
+        let region = MKCoordinateRegion(center: mapView.userLocation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        mapView.setRegion(mapView.regionThatFits(region), animated: true)
+    }
+    
+    @IBAction func showLocations(){
+        let theRegion = region(for: locations)
+        mapView.setRegion(theRegion, animated: true)
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // click the annotation button to go to the EditLoction view, also pass the correct location to locationToEdit property in EditLocation view.
+        if segue.identifier == "EditLocation" {
+            let controller = segue.destination as! LocationDetailsViewController
+            controller.managedObjectContext = managedObjectContext
+            
+            let button = sender as! UIButton
+            let location = locations[button.tag]
+            controller.locationToEdit = location
+        }
+    }
+    
+    // MARK: - Private Methods
+    // show all marked locations in the map view, this time we don't sort the fetchRequest.
+    // try! get rid of the do-try-catch block, if you think it can never fail, you can use this way to make the code more clearer
+    // this method first remove all annotations(pin) from the map then adds them back
+    func updateLocations(){
+        mapView.removeAnnotations(locations)
+        let entity = Location.entity()
+        let fetchRequest = NSFetchRequest<Location>()
+        fetchRequest.entity = entity
+        locations = try! managedObjectContext.fetch(fetchRequest)
+        mapView.addAnnotations(locations)
+    }
+    
+    func region(for annotations: [MKAnnotation]) -> MKCoordinateRegion {
+        let region: MKCoordinateRegion
+        
+        switch annotations.count {
+        case 0:
+            region = MKCoordinateRegion(center: mapView.userLocation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        case 1:
+            let annotation = annotations[annotations.count - 1]
+            region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+            
+        default:
+            var topLeft = CLLocationCoordinate2D(latitude: -90, longitude: 180)
+            var bottomRight = CLLocationCoordinate2D(latitude: 90, longitude: -180)
+            
+            for annotation in annotations {
+                topLeft.latitude = max(topLeft.latitude, annotation.coordinate.latitude)
+                topLeft.longitude = min(topLeft.longitude, annotation.coordinate.longitude)
+                bottomRight.latitude = min(bottomRight.latitude, annotation.coordinate.latitude)
+                bottomRight.longitude = max(bottomRight.longitude, annotation.coordinate.longitude)
+            }
+            
+            let center = CLLocationCoordinate2D(latitude: topLeft.latitude - (topLeft.latitude - bottomRight.latitude) / 2, longitude: topLeft.longitude - (topLeft.longitude - bottomRight.longitude) / 2)
+            
+            let extraSpace = 1.1
+            let span = MKCoordinateSpan(latitudeDelta: abs(topLeft.latitude - bottomRight.latitude) * extraSpace, longitudeDelta: abs(topLeft.longitude - bottomRight.longitude) * extraSpace)
+            
+            region = MKCoordinateRegion(center: center, span: span)
+        }
+        return mapView.regionThatFits(region)
+    }
+    
+    @objc func showLocationDetails(_ sender: UIButton) {
+        performSegue(withIdentifier: "EditLocation", sender: sender)
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // 1 make sure annotation is a type of Location object, guard keyword works like 'if'.
+        // if 'annotation is Location' is true, then we ignore the 'else' code block, if it is false or nil, then we execute the else code block.
+        guard annotation is Location else {
+            return nil
+        }
+        // 2 reuse annotation object
+        let identifier = "Location"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+        if annotationView == nil {
+            let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            // 3 customize annotations
+            pinView.isEnabled = true
+            pinView.canShowCallout = true
+            pinView.animatesDrop = false
+            pinView.pinTintColor = UIColor(red: 0.32, green: 0.82, blue: 0.4, alpha: 1)
+            // 4 create a button, when clicked, will perform showLocationDetails method, and this button is hooked with an annotation's accessoryView, so everytime when we click an annoatation, we actually clicked a button and called a method.
+            let rightButton = UIButton(type: .detailDisclosure)
+            rightButton.addTarget(self, action: #selector(showLocationDetails), for: .touchUpInside)
+            pinView.rightCalloutAccessoryView = rightButton
+            annotationView = pinView
+        }
+        
+        if let annotationView = annotationView {
+            annotationView.annotation = annotation
+            // 5 set button's tag equals to annotation's index in the Location array, so next we could find the right annotation object.
+            let button = annotationView.rightCalloutAccessoryView as! UIButton
+            if let index = locations.firstIndex(of: annotation as! Location) {
+                button.tag = index
+            }
+        }
+        
+        return annotationView
+    }
+}
