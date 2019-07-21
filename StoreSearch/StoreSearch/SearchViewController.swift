44e93e1e404b3956f430adb61ba19530a512c405
Yuan Cheng <yuan@unicard.com.au>
2019-07-21 11:29:05 +1000

    added StoreSearch project

---
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by yuancheng on 21/7/19.
+//  Copyright © 2019 yuancheng. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
