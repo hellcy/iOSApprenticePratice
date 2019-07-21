021aa1772e37625a58ba649c2005f46cbecaeb49
Yuan Cheng <yuan@unicard.com.au>
2019-07-21 12:15:22 +1000

    first section

---
+++ StoreSearch/StoreSearch/SearchResults.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResults.swift
+//  StoreSearch
+//
+//  Created by yuancheng on 21/7/19.
+//  Copyright © 2019 yuancheng. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
--- StoreSearch/StoreSearch/SearchViewController.swift
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -10,11 +10,95 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // data model
+    var searchResults = [SearchResult]()
+    var hasSearched = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // add a 64 points margin to the top, 20 for the status bar and 44 for the search bar
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0,
+                                              bottom: 0, right: 0)
     }
 
 
 }
 
+// MRAK: - Search Bar Delegate Methods
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // dismiss the keyboard
+        searchBar.resignFirstResponder()
+        // empty the previous stored data every time user click Search button
+        searchResults = []
+        if searchBar.text! != "justin bieber" {
+            for i in 0...2 {
+                let searchResult = SearchResult()
+                searchResult.name = String(format: "Fake Result %d for", i)
+                searchResult.artistName = searchBar.text!
+                searchResults.append(searchResult)
+            }
+        }
+        hasSearched = true
+        tableView.reloadData()
+    }
+    
+    // attach the search bar to the top of the screen
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
+
+// MARK: - Table View Delegate Methods
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    // number of cells to display
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !hasSearched {
+            return 0
+        } else if searchResults.count == 0 {
+            return 1
+        } else {
+            return searchResults.count
+        }
+    }
+    
+    // assign data to cells to display
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        var cell:UITableViewCell! = tableView.dequeueReusableCell(
+            withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle,
+                                   reuseIdentifier: cellIdentifier)
+        }
+        
+        // check if searchResults returns no data (search failed)
+        if searchResults.count == 0 {
+            cell.textLabel!.text = "(Nothing found)"
+            cell.detailTextLabel!.text = ""
+        } else {
+            // add searchResult values to cell
+            let searchResult = searchResults[indexPath.row]
+            cell.textLabel!.text = searchResult.name
+            cell.detailTextLabel!.text = searchResult.artistName
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // make sure user can't select cell when there is no searchResults
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if searchResults.count == 0 {
+            return nil
+        } else {
+            return indexPath
+        }
+    }
+}
